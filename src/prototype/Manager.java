73c914fa87a7ea247b8170ec7ab828fdef059041
PeonyF <1621139@gmail.com>
2020-01-15 00:58:23 +0900

    update Prototype java pattern.

--- src/prototype/Manager.java
+++ src/prototype/Manager.java
@@ -2,9 +2,15 @@ package prototype;
 
 import java.util.HashMap;
 
-public class Manager {
+class Manager {
     private HashMap showcase = new HashMap();
-    public void register(String name, Product proto){
-        showcase.put(name,proto);
+
+    void register(String name, Product proto) {
+        showcase.put(name, proto);
+    }
+
+    Product create(String protoname) {
+        Product p = (Product) showcase.get(protoname);
+        return p.createClone();
     }
 }
